90227d3c6da9916a1a03b8ffdf7f7fa7487dffc5
Cezary Chlebda <cezary1539@gmail.com>
2024-02-16 11:00:10 +0100

    project base structure set up
    created few empty scripts,
    created agent prefab,
    arranged folders and scene

---
+++ Assets/_InternalAssets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+
+    private static T instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (T)FindObjectOfType(typeof(T));
+                if (instance == null)
+                {
+                    string goName = typeof(T).ToString();
+                    GameObject go = GameObject.Find(goName);
+                    if (go == null)
+                    {
+                        go = new GameObject();
+                        go.name = goName;
+                    }
+                    instance = go.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+}
