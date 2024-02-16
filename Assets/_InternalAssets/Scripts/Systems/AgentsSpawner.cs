3943300f6211ae6be59bb0f8523613515983dc35
Cezary Chlebda <cezary1539@gmail.com>
2024-02-16 12:56:30 +0100

    added base logic to agent spawner
    few agents spawns initially and then one spawns every few seconds

--- Assets/_InternalAssets/Scripts/Systems/AgentsSpawner.cs
+++ Assets/_InternalAssets/Scripts/Systems/AgentsSpawner.cs
@@ -1,14 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class AgentsSpawner : MonoBehaviour
 {
     [SerializeField]
-    private int minStartAgentsCount = 3;
-    [SerializeField]
-    private int maxStartAgentsCount = 5;
-    [SerializeField]
     private GameObject agentPrefab;
 
+    [Space]
+    [Header("How many agents should initially spawn")]
+    [SerializeField] [Min(0)]
+    private int minStartAgentsCount = 3;
+    [SerializeField] [Min(0)]
+    private int maxStartAgentsCount = 5;
+
+    [Space]
+    [Header("How often should new agent be spawned")]
+    [SerializeField] [Min(.01f)]
+    private float minSpawnTime = 2;
+    [SerializeField] [Min(.01f)]
+    private float maxSpawnTime = 6;
+
+    [Space]
+    [SerializeField] [Range(0, 100)]
+    [Description("Controls how far away from center of map agent can walk away and spawn")]
+    private float agentMaxDistance = 10;
+    [SerializeField] [Range(1, 5)]
+    private float agentsSpawnHeight = 3;
+
+    [Space]
+    [SerializeField]
+    private int maxAgentsCount = 30;
+
+    [SerializeField]
+    private float timeUntilNextSpawn = 1000;
+
+    public static int CurrentAgentsCount = 0;
+
+    private void Start()
+    {
+        int startAgentsCount = Random.Range(minStartAgentsCount, maxStartAgentsCount);
+
+        for (int i = 0; i < startAgentsCount; i++)
+        {
+            SpawnAgent();
+        }
+
+        timeUntilNextSpawn = Random.Range(minSpawnTime, maxSpawnTime);
+    }
+
+    private void Update()
+    {
+        if (CurrentAgentsCount > maxAgentsCount)
+            return;
+
+        if (timeUntilNextSpawn > 0)
+        {
+            timeUntilNextSpawn -= Time.deltaTime;
+        }
+        else
+        {
+            SpawnAgent();
+            timeUntilNextSpawn = Random.Range(minSpawnTime, maxSpawnTime);
+        }
+    }
+
+    public void SpawnAgent()
+    {
+        Vector3 spawnPosition = new(Random.Range(-agentMaxDistance, agentMaxDistance), agentsSpawnHeight, Random.Range(-agentMaxDistance, agentMaxDistance));
+
+        ObjectPoolManager.SpawnObject(agentPrefab, spawnPosition, Quaternion.identity);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(new Vector3(0, 0, 0), new Vector3(agentMaxDistance * 2, 0, agentMaxDistance * 2));
+    }
 }
